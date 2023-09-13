247171918e8ce5b3d23bae1d078ea635ed43f588
Giulio Yongco <yongcogiulio125@gmail.com>
2023-09-13 22:34:12 +0800

    clearer tutorial

--- Basic_01/type_hinting.py
+++ Basic_01/type_hinting.py
@@ -3,14 +3,21 @@
 # Date            : ur my date uwu
 # HEADER ------------------------------------------------------------------------------------------
 
-def add(x:int , y:int)->int:
+def add_concat(x:int | str , y:int | str)->(int|str):
+    # these are called union operators me thinks
+    # only works python 3.10 above
     return x + y
+
+def only_multiply(x:int , y:int )->int:
+    return x*y*69
 
 # MAIN --------------------------------------------------------------------------------------------
 print("\nSTART ----------------------------------\n")
 
-result = add(79, 6)
-
-print(result)
+print(add_concat(6, 9), add_concat("yes", "no"))
+print(only_multiply(7, 2))
+# only gives warning
+# but mostly for good practice
+# it also makes documentation easier
 
 print("\nEND ------------------------------------\n")
